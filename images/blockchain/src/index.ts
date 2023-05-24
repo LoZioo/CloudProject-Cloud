cfc80d671ff6ea3e9554cb5b6fa9a81a2849a779
LoZioo <60042676+LoZioo@users.noreply.github.com>
2023-05-24 12:56:50 +0200

    feat: saveToFile

--- images/blockchain/src/index.ts
+++ images/blockchain/src/index.ts
@@ -28,6 +28,35 @@ function is_CloudBlock_t(obj: any): obj is CloudBlock_t {
 	return keys.every(key => key in obj);
 }
 
+// Functions.
+import fs from "fs";
+import { access, readFile, writeFile } from "fs/promises";
+
+/**
+ * Save the specified block to the specified json file (only the timestamp and the hash).
+ */
+async function saveToFile(block: CloudBlock_t, file: string): Promise<void> {
+	// Open existing file.
+	try {
+		await access(file, fs.constants.F_OK);
+	}
+
+	// Create new file.
+	catch (e){
+		await writeFile(file, JSON.stringify([]), "utf-8");
+	}
+
+	finally {
+		const content: Array<unknown> = JSON.parse(await readFile(file, "utf-8"));
+		content.push({
+			timestamp:	block.timestamp,
+			hash:				block.hash
+		});
+
+		await writeFile(file, JSON.stringify(content, null, 2), "utf-8");
+	}
+}
+
 // App log.
 import { format } from  "util";
 import { stdout, stderr } from "process";
@@ -50,6 +79,16 @@ catch(e){
 
 const HTTP_ADDRESS = env.HTTP_ADDRESS as string;
 
+try {
+	assert("DB_FILE" in env);
+}
+catch(e){
+	log("DB_FILE envroiment variable not set, exiting...", "Error", stderr);
+	process.exit(1);
+}
+
+const DB_FILE = env.DB_FILE as string;
+
 // Express server.
 import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
@@ -65,6 +104,8 @@ app.get("/", (req: Request, res: Response) => {
 			{
 				endpoint:			"/add",
 				method:				"post",
+				body:					"application/json",
+				bodyType:			"CloudBlock_t",
 				description:	"Add the hash and the timestamp of a CloudBlock_t object to the blockchain."
 			}
 		]
@@ -75,7 +116,7 @@ app.get("/", (req: Request, res: Response) => {
 	res.send(JSON.stringify(endpoints));
 });
 
-app.post("/add-block", (req: Request, res: Response) => {
+app.post("/add-block", async (req: Request, res: Response) => {
 	const block: CloudBlock_t = req.body;
 
 	try {
@@ -88,6 +129,10 @@ app.post("/add-block", (req: Request, res: Response) => {
 	}
 
 	log(format("Received block from %s:%d.", req.ip, req.socket.remotePort));
+
+	log("Saving the received block.");
+	await saveToFile(block, DB_FILE);
+
 	res.sendStatus(200);
 });
 
