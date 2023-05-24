30a792536b28f402382d28cde531bbf0fb94976e
LoZioo <60042676+LoZioo@users.noreply.github.com>
2023-05-24 12:15:02 +0200

    feat: /add-block

--- images/blockchain/src/index.ts
+++ images/blockchain/src/index.ts
@@ -1,6 +1,33 @@
 // HTTP port.
 const HTTP_PORT = 5090;
 
+// Cloud block.
+interface CloudBlock_t {
+	VA:					Array<number>,
+	W:					Array<number>,
+
+	timestamp:	string,
+	hash:				string,
+}
+
+/**
+ * Check if obj is a CloudBlock_t object (check keys only, not values).
+ * @param obj
+ * @returns obj is CloudBlock_t
+ */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function is_CloudBlock_t(obj: any): obj is CloudBlock_t {
+	const testObj: CloudBlock_t = {
+		VA: [],
+		W: [],
+		timestamp: "",
+		hash: ""
+	};
+
+	const keys = Object.keys(testObj);
+	return keys.every(key => key in obj);
+}
+
 // App log.
 import { format } from  "util";
 import { stdout, stderr } from "process";
@@ -24,16 +51,48 @@ catch(e){
 const HTTP_ADDRESS = env.HTTP_ADDRESS as string;
 
 // Express server.
-import express, { Express, Request, Response } from "express";
+import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
 
 const app = express();
+app.use(bodyParser.json());		// Now Express can decode the application/json body inside req.body.
 
-app.get('/', (req: Request, res: Response) => {
-	res.send('Express + TypeScript Server');
+app.get("/", (req: Request, res: Response) => {
+	// Available endpoints.
+	const endpoints = {
+		service: "add-block",
+		endpoints: [
+			{
+				endpoint:			"/add",
+				method:				"post",
+				description:	"Add the hash and the timestamp of a CloudBlock_t object to the blockchain."
+			}
+		]
+	};
+
+	// Send response.
+	res.contentType("application/json");
+	res.send(JSON.stringify(endpoints));
+});
+
+app.post("/add-block", (req: Request, res: Response) => {
+	const block: CloudBlock_t = req.body;
+
+	try {
+		assert(is_CloudBlock_t(block));
+	}
+
+	catch(e){
+		res.sendStatus(400);
+		return;
+	}
+
+	log(format("Received block from %s:%d.", req.ip, req.socket.remotePort));
+	res.sendStatus(200);
 });
 
 const server = app.listen(HTTP_PORT, HTTP_ADDRESS, () => {
-	log(format("Server is running at http://%s:%d", HTTP_ADDRESS, HTTP_PORT));
+	log(format("Server is running at http://%s:%d.", HTTP_ADDRESS, HTTP_PORT));
 });
 
 // Graceful shutdown.
@@ -46,3 +105,6 @@ function gracefulShutdown(): void {
 	server.close();
 	process.exit(0);
 }
+
+// Main.
+log("I'm the blockchain!");
