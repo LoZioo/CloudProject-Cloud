3568c57e8a4b4152f953bd47664f943bdc7caaca
LoZioo <60042676+LoZioo@users.noreply.github.com>
2023-05-31 16:23:28 +0200

    feat: setup_kube

--- config.sh
+++ config.sh
@@ -7,6 +7,7 @@ export USER="ubuntu"
 
 # SSH proxy.
 export SERVER_0_ADDRESS="90.147.185.7"
+export SERVER_0_NET_ADDRESS="192.168.0.248"
 
 export SERVER_1_LOCAL_ADDRESS="127.0.0.2"
 export SERVER_1_REMOTE_ADDRESS="192.168.0.100"
@@ -22,3 +23,12 @@ export SERVER_BC_ADDRESS="90.147.185.8"
 # Docker settings.
 export BUILD_PLATFORMS="linux/amd64"
 export DOCKERHUB_USER="lozioo"
+
+# Kubernetes (see ./setup_kube/config.sh).
+KHOSTS_NETWORK="192.168.0.0/24"
+
+KHOSTS[m]=$SERVER_0_NET_ADDRESS
+KHOSTS[w1]=$SERVER_1_REMOTE_ADDRESS
+KHOSTS[w2]=$SERVER_2_REMOTE_ADDRESS
+
+KUSER=$USER
